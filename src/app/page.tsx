fe1358651d430a75491338475f07173bf8700cca
Marta Kruneva <marta.kruneva@gmail.com>
2024-11-18 23:21:46 +0200

    add user search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 // import styles from "./page.module.css";
 import { UserProvider } from "./contexts/user-context";
 import { UserTable } from "./components/user-table";
+import { UserSearch } from "./components/user-search";
 
 export default function Home() {
   return (
     <UserProvider>
       <div className="users-container">
         <h1>Users Table</h1>
-        {/* <UsersSearch /> */}
+        <UserSearch />
         <UserTable />
       </div>
     </UserProvider>
